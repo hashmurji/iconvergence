9132c1b845a78c75f42a1bc2cfb665ed465e2bbf
Harshad Murji <harshadmurji@Mac.powerhub>
2026-06-23 17:43:34 +0100

    Fix: explicit vite root and vercel config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 2000,
+    outDir: 'dist',
   },
 })
